8e435aa986cbeac45e10aa09c8df15ed6061e34b
gitblossom <dija.uzumaki@gmail.com>
2024-02-29 11:47:50 -0800

    updated

--- 131-heap_insert.c
+++ 131-heap_insert.c
@@ -1,68 +1,46 @@
 #include "binary_trees.h"
-
+#include "102-binary_tree_is_complete.c"
 /**
-* heap_insert - inserts a value in Max Binary Heap
-* @root: a double pointer to the root node of the Heap to insert the value
-* @value: the value to store in the node to be inserted
-*
-* Return: a pointer to the created node
-*         NULL on failure
-*/
-heap_t *heap_insert(heap_t **root, int value)
+* check_max - goes through a binary tree cheking ropt as max value
+* @tree: pointer to the root
+* Return: 1 if all nodes are max, 0 in other cases
+**/
+int check_max(const binary_tree_t *tree)
 {
-heap_t *tree, *new, *flip;
-int size, leaves, sub, bit, level, tmp;
+int tmp1 = 1, tmp2 = 1;
 
-if (!root)
-return (NULL);
-if (!(*root))
-return (*root = binary_tree_node(NULL, value));
-tree = *root;
-size = binary_tree_size(tree);
-leaves = size;
-for (level = 0, sub = 1; leaves >= sub; sub *= 2, level++)
-leaves -= sub;
-/* subtract all nodes except for bottom-most level */
-
-for (bit = 1 << (level - 1); bit != 1; bit >>= 1)
-tree = leaves & bit ? tree->right : tree->left;
-/*
-* Traverse tree to first empty slot based on the binary
-* representation of the number of leaves.
-* Example -
-* If there are 12 nodes in a complete tree, there are 5 leaves on
-* the 4th tier of the tree. 5 is 101 in binary. 1 corresponds to
-* right, 0 to left.
-* The first empty node is 101 == RLR, *root->right->left->right
-*/
-
-new = binary_tree_node(tree, value);
-leaves & 1 ? (tree->right = new) : (tree->left = new);
-
-flip = new;
-for (; flip->parent && (flip->n > flip->parent->n); flip = flip->parent)
+if (!tree)
+return (0);
+if (!tree->left && !tree->right)
+return (1);
+if (tree->left)
 {
-tmp = flip->n;
-flip->n = flip->parent->n;
-flip->parent->n = tmp;
-new = new->parent;
+if (tree->n <= tree->left->n)
+return (0);
+tmp1 = check_max(tree->left);
 }
-/* Flip values with parent until parent value exceeds new value */
-
-return (new);
+if (tree->right)
+{
+if (tree->n <= tree->right->n)
+return (0);
+tmp2 = check_max(tree->right);
 }
-
+return (tmp1 && tmp2);
+}
 /**
-* binary_tree_size - measures the size of a binary tree
-* @tree: tree to measure the size of
-*
-* Return: size of the tree
-*         0 if tree is NULL
-*/
-size_t binary_tree_size(const binary_tree_t *tree)
+* binary_tree_is_heap - checks if a binary tree is heap
+* @tree: pointer to the node
+* Return: 1 in case BTS /  0 otherwise
+**/
+int binary_tree_is_heap(const binary_tree_t *tree)
 {
+int tmp;
+
 if (!tree)
 return (0);
 
-return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+tmp = binary_tree_is_complete(tree);
+if (!tmp)
+return (0);
+return (check_max(tree));
 }
